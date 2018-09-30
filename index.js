9cb5ced9d18b4b336bf53211a3b5a820e874f1ab
sergio marelli <sergio.mrll@gmail.com>
2018-09-30 13:08:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,7 +61,11 @@ function total() {
 }
 
 function removeFromCart(item) {
-    
+    for (var i = 0; i < cart.length; i++) {
+        if (cart[i].itemName === item) {
+            cart.splice(i,1)
+        }
+    }
     return "That item is not in your cart."
 }
 

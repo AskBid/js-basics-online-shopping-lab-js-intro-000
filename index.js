2737e687a159177226ef3907ea82449c5c2d5400
sergio marelli <sergio.mrll@gmail.com>
2018-09-30 13:18:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -75,5 +75,10 @@ function placeOrder(cardNumber) {
         return "Sorry, we don't have a credit card on file for you."
     }
 
+    var totPrice = total()
     cart = []
+    
+    return `Your total cost is $${}, which will be charged to the card ${cardNumber}.`
+
+    
 }
